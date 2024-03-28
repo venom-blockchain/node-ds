37d29cf75c33aef6faab2c160c2f459c5dbc2700
bxvenom <140181889+bxvenomgithub@users.noreply.github.com>
2024-03-28 14:39:16 +0300

    Add update script

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dc=""
+
+docker-compose version >/dev/null 2>/dev/null
+if [[ "$?" == "0" ]]; then
+    dc="docker-compose"
+fi
+
+docker compose version >/dev/null 2>/dev/null
+if [[ "$?" == "0" ]]; then
+    dc="docker compose"
+fi
+
+if [[ "$dc" == "" ]]; then
+    echo "Error: functioning docker compose or docker-compose not found"
+    exit 1
+fi
+
+echo "Update Node"
+$dc -f deploy/kafka-producer/docker-compose.yml build --no-cache
+$dc -f deploy/kafka-producer/docker-compose.yml up -d --force-recreate
